73e464970e2180ca083dd18cc275d644d17c552c
Vidy <vidy.thatte@gmail.com>
2019-04-10 10:13:29 -0700

    Updates

---
+++ precache-manifest.e308d353aa5370ef645f4a97d75318aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/embed-tool/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1b98eaa1d8fba520d7af",
+    "url": "/embed-tool/static/js/runtime~main.00c80b38.js"
+  },
+  {
+    "revision": "f1100017f80ecb01a78f",
+    "url": "/embed-tool/static/js/main.debcf5f0.chunk.js"
+  },
+  {
+    "revision": "487a16d90ffb9b3a0d71",
+    "url": "/embed-tool/static/js/2.1373f3a0.chunk.js"
+  },
+  {
+    "revision": "f1100017f80ecb01a78f",
+    "url": "/embed-tool/static/css/main.2b76bc4f.chunk.css"
+  },
+  {
+    "revision": "7ce9911823a6b17e0b1b12e0d63c9154",
+    "url": "/embed-tool/index.html"
+  }
+];
